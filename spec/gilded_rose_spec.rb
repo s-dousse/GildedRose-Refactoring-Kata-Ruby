e587abd3b11ce1ee78f938a5f0b4c20629aaf342
Sarah <dousse.sarah@gmail.com>
2022-02-03 00:49:42 +0000

    add test for 'normal item' when sell by dated is passed

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -15,14 +15,21 @@ describe GildedRose do
     end
 
     context 'normal item' do
-      let(:apple) { Item.new('Apple', 1, 4) }
-      let(:gilded_rose) { GildedRose.new([apple]) }
+      apple = Item.new('Apple', 1, 4)
+      gilded_rose = GildedRose.new([apple])
 
-      it "decrease the quality by 1 when a day passes" do
+      it "decrease the quality by 1 before sell by date" do
         apple_updated = gilded_rose.update_quality[0]
 
         expect(apple_updated.sell_in).to eq 0
         expect(apple_updated.quality).to eq 3
+      end
+
+      it "decrease the quality by 2 when sell by date is passed" do
+        apple_updated = gilded_rose.update_quality[0]
+
+        expect(apple_updated.sell_in).to eq -1
+        expect(apple_updated.quality).to eq 1
       end
     end
   end
