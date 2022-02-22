e5835e146ffb64768eb2d9746cb4e7e1f58e95db
Sarah <dousse.sarah@gmail.com>
2022-02-22 15:27:29 +0000

    refactor using a switch statement

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -7,40 +7,32 @@ class GildedRose
   def update_quality
     @items.each do |item|
       unless item.name == 'Sulfuras, Hand of Ragnaros'
-    # if normal item decrese quality by 1 
-        if item.name != 'Aged Brie' and item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          # normal item if quality < 0 no change
-          item.quality = item.quality - 1 if item.quality > 0
-        elsif item.quality < 50
-          # aged brie and backstage passes +1
-          item.quality = item.quality + 1
-          if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-            # backstage passes + 1 if sell_in <
-            item.quality = item.quality + 1 if item.sell_in < 11 && (item.quality < 50)
-            # backstage passes + 1 if sell_in <
-            item.quality = item.quality + 1 if item.sell_in < 6 && (item.quality < 50)
+          case item.name
+            when 'Aged Brie'
+              item.quality += 1 if (item.quality < 50)
+            when 'Backstage passes to a TAFKAL80ETC concert'
+              item.quality += 1 if (item.quality < 50)
+              item.quality += 1 if item.sell_in < 11 && (item.quality < 50)
+              item.quality += 1 if item.sell_in < 6 && (item.quality < 50)
+            else # normal items
+              item.quality -= 1 if item.quality > 0
           end
-        end
-    # end of normal item decrese quality by 1 
-
         # a day passes
-        item.sell_in = item.sell_in - 1
+        item.sell_in -= 1
         
-    # if sell_by date is passed
+        # if sell_by date is passed
         if item.sell_in < 0
-          if item.name != 'Aged Brie'
-            if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-              # if normal item decrese quality by 1
-              item.quality = item.quality - 1 if item.quality > 0
-            else
-              # if backstage passes  quality == 0
-              item.quality = item.quality - item.quality
-            end
-          elsif item.quality < 50
-            item.quality = item.quality + 1
+          case item.name
+            when 'Aged Brie'
+              item.quality += 1 if (item.quality < 50)
+              item.quality += 1 if (item.quality < 49)
+            when 'Backstage passes to a TAFKAL80ETC concert'
+              item.quality -= item.quality
+            else # normal items
+              item.quality -= 1 if item.quality > 0
           end
         end
-    # end of if sell_by date is passed
+        # end of if sell_by date is passed
       end
     end
   end
